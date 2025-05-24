b56522f18e0fa57b87dd4c46f138d5a926a61915
justin wangsa <justinwangsa9@gmail.com>
2025-05-24 21:09:52 +0800

    ISENG

--- src/index.php
+++ src/index.php
@@ -1,11 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sign In</title>
+  <link rel="stylesheet" href="./assets/css/login.css">
+  <link rel="shortcut icon" href="./assets/images/fav.png" type="image/x-icon">
 </head>
 <body>
-    <H1>test</H1>
+  <div class="container">
+    <center> <img src="./assets/images/logo.png" width="60%" alt=""> </center>
+
+    <form class="login-form" action="./login.php" method="post">
+      
+      <div>
+        <label for="email">Email </label>
+        <input id="email" type="email" name="email" required />
+      </div>
+
+      <div>
+        <label for="password">Password </label>
+        <input id="password" type="password" name="password" required />
+      </div>
+
+      <button class="btn btn--form" type="submit" value="Log in">Sign In</button>
+
+    </form>
+
+  </div>
 </body>
 </html>
