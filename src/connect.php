90659a49b31524ff925582563049415b720c7c40
bertrand252 <bertrandleonard736@gmail.com>
2025-05-27 09:21:00 +0800

    input image

--- src/connect.php
+++ src/connect.php
@@ -4,9 +4,9 @@ $username = "root";
 $password = "";           
 $dbname = "inventory";       
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$con = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
 }
 ?>
--- src/test_img.php
+++ src/test_img.php
@@ -1,11 +1,53 @@
+<?php
+    include('connect.php');
+    if(isset($_POST['submit'])){
+        $file_name = $_FILES['image']['name'];
+        $tempname = $_FILES['image']['tmp_name'];
+        $folder = 'image/'.$file_name;
+
+        // $query = mysqli_query($con, "insert into items(product_image) values ('$file_name')");
+
+        if(move_uploaded_file($tempname, $folder)){
+                $query = mysqli_query($con, "INSERT INTO items (
+                    product_code,
+                    product_remarks,
+                    product_quantity,
+                    product_image
+                ) VALUES (
+                    'ex',
+                    'ex',
+                    1,
+                    '$file_name'
+                )");
+            echo "<h2>File Upload Successfully<h2>";
+        }else{
+            echo "<h2>File NOT Uploaded<h2>";
+        }
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Document</title> 
+    
 </head>
 <body>
-    <form method="post" enctype=""></form>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <br/><br/>
+        <button type="submit" name="submit">submit</button>
+    </form>
+    <div>
+        <?php
+            $res = mysqli_query($con, "select * from items");
+            while($row = mysqli_fetch_assoc($res)){
+            ?>
+            <img src = "image/<?php echo $row["product_image"]; ?>" />
+        <?php } ?>
+</div>
 </body>
 </html>
