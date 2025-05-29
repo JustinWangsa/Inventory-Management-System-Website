e23c04291e3c79b1f10eac257d33541b78445ef3
bertrand252 <bertrandleonard736@gmail.com>
2025-05-29 09:32:45 +0800

    fixed delete bug

--- src/delete.php
+++ src/delete.php
@@ -5,8 +5,10 @@ include('connect.php');
 if(isset($_GET['id'])){ // for check if ID is set
     $result = mysqli_query($conn, "DELETE FROM items WHERE id=" . $_GET['id']);
     if ($result == true){
-        echo "<script>alert('Successfully Deleted');</script>"; 
-        header("Location: index.php");
+        echo "<script>
+                alert('Successfully Deleted');
+                window.location.href = 'index.php';
+            </script>"; 
         exit();
         }
     }
